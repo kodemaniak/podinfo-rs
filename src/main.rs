f4c3484583a6b7e6e06604185afa061a78d81100
Carsten Saathoff <carsten@kodemaniak.de>
2022-01-16 17:28:10 +0100

    Refactoring: Moved z-pages to dedicated module.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
-use axum::{
-    extract::Extension,
-    http::StatusCode,
-    routing::{get, post},
-    AddExtensionLayer, Router,
-};
+use axum::Router;
 use envconfig::Envconfig;
+use podinfo_rs::z;
 use podinfo_rs::{config::Configuration, error::PodinfoError};
-use std::{
-    net::{IpAddr, SocketAddr},
-    sync::{Arc, RwLock},
-};
+use std::net::{IpAddr, SocketAddr};
 
 #[tokio::main]
 async fn main() -> Result<(), PodinfoError> {
     let config = Configuration::init_from_env()?;
 
-    let app = Router::new().nest("/_z", z_routes());
+    let app = Router::new().nest("/_z", z::z_routes());
 
     axum::Server::bind(&SocketAddr::new(
         IpAddr::V4(config.bind_ip),
@@ -26,52 +19,4 @@ async fn main() -> Result<(), PodinfoError> {
     .unwrap();
 
     Ok(())
-}
-
-fn z_routes() -> Router {
-    Router::new()
-        .route("/healthz", get(get_healthz))
-        .route("/readyz", get(get_readyz))
-        .route("/readyz/enable", post(enable_readyz))
-        .route("/readyz/disable", post(disable_readyz))
-        .layer(AddExtensionLayer::new(SharedReadyzState::default()))
-}
-
-async fn get_healthz() -> StatusCode {
-    StatusCode::OK
-}
-
-async fn get_readyz(Extension(readyz_state): Extension<SharedReadyzState>) -> StatusCode {
-    // TODO: is it ok to unwrap here?
-    if readyz_state.read().unwrap().enabled {
-        StatusCode::OK
-    } else {
-        StatusCode::SERVICE_UNAVAILABLE
-    }
-}
-
-async fn enable_readyz(Extension(readyz_state): Extension<SharedReadyzState>) -> StatusCode {
-    // TODO: is it ok to unwrap here?
-    readyz_state.write().unwrap().enabled = true;
-
-    StatusCode::OK
-}
-
-async fn disable_readyz(Extension(readyz_state): Extension<SharedReadyzState>) -> StatusCode {
-    // TODO: is it ok to unwrap here?
-    readyz_state.write().unwrap().enabled = false;
-
-    StatusCode::OK
-}
-
-type SharedReadyzState = Arc<RwLock<ReadyzState>>;
-
-struct ReadyzState {
-    enabled: bool,
-}
-
-impl Default for ReadyzState {
-    fn default() -> Self {
-        Self { enabled: true }
-    }
 }
---
+++ src/z.rs
@@ -0,0 +1,53 @@
+use axum::{
+    extract::Extension,
+    http::StatusCode,
+    routing::{get, post},
+    AddExtensionLayer, Router,
+};
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
+pub fn z_routes() -> Router {
+    Router::new()
+        .route("/healthz", get(get_healthz))
+        .route("/readyz", get(get_readyz))
+        .route("/readyz/enable", post(enable_readyz))
+        .route("/readyz/disable", post(disable_readyz))
+        .layer(AddExtensionLayer::new(SharedReadyzState::default()))
+}
+
+async fn get_healthz() -> StatusCode {
+    StatusCode::OK
+}
+
+async fn get_readyz(Extension(readyz_state): Extension<SharedReadyzState>) -> StatusCode {
+    if readyz_state.read().await.enabled {
+        StatusCode::OK
+    } else {
+        StatusCode::SERVICE_UNAVAILABLE
+    }
+}
+
+async fn enable_readyz(Extension(readyz_state): Extension<SharedReadyzState>) -> StatusCode {
+    readyz_state.write().await.enabled = true;
+
+    StatusCode::OK
+}
+
+async fn disable_readyz(Extension(readyz_state): Extension<SharedReadyzState>) -> StatusCode {
+    readyz_state.write().await.enabled = false;
+
+    StatusCode::OK
+}
+
+type SharedReadyzState = Arc<RwLock<ReadyzState>>;
+
+struct ReadyzState {
+    enabled: bool,
+}
+
+impl Default for ReadyzState {
+    fn default() -> Self {
+        Self { enabled: true }
+    }
+}
