afb72b36ac33a7d0a5d1946a57a067052a31794a
Carsten Saathoff <carsten@kodemaniak.de>
2022-02-17 20:17:06 +0100

    Tracing and logging support (#18)
    * Added tracing and logging support.
    
    * Using anyhow for better error handling. Using TraceLayer on top level router only.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
+use anyhow::Result;
 use axum::routing::post;
 use axum::Router;
 use envconfig::Envconfig;
+use log::info;
+use podinfo_rs::config::Configuration;
 use podinfo_rs::echo::echo;
 use podinfo_rs::z;
-use podinfo_rs::{config::Configuration, error::PodinfoError};
 use std::net::{IpAddr, SocketAddr};
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
-async fn main() -> Result<(), PodinfoError> {
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
     let config = Configuration::init_from_env()?;
 
     let app = Router::new()
         .nest("/_z", z::z_routes())
-        .route("/echo", post(echo));
+        .route("/echo", post(echo))
+        .layer(TraceLayer::new_for_http());
 
+    info!("Starting podinfo-rs...");
     axum::Server::bind(&SocketAddr::new(
         IpAddr::V4(config.bind_ip),
         config.bind_port,
